b4d138d2898faefedf70a71f391f6d85f96863e7
LucasFA <lucaslanzarote@gmail.com>
2022-03-20 12:43:48 +0100

    Move inputBoard from config to main

--- src/config.h
+++ src/config.h
@@ -1,8 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#include<cstdint>
-#include<string>
+#include <cstdint>
+
 const std::uint8_t GRID_SIZE = 9;
-const std::string inputBoard = "030005086 000407300 010080400 090001000 000000027 700000108 805102000 074500001 100700030";
 #endif 
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include "config.h"
 
 int main() {
+	const std::string inputBoard = 
+		"030005086 000407300 010080400 090001000 000000027 700000108 805102000 074500001 100700030";
 	Board board(inputBoard); // defined in config.h
 	
 	return 0;
